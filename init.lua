a3a991734980382dc965a2e24ceb5fe2b964fe35
mohnishsalian0 <mohnishsalian02@gmail.com>
2024-10-20 01:33:02 +0530

    Backspace indent and js prettier

--- init.lua
+++ init.lua
@@ -162,6 +162,7 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.backspace = { 'start', 'eol', 'indent' }
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
